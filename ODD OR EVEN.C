92d40970ad97ad93d09aed74673a12985b50ca9b
kohli17 <35589866+kohli17@users.noreply.github.com>
2018-02-12 15:49:46 +0530

    Create ODD OR EVEN.C
    print the odd or even number

---
+++ ODD OR EVEN.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h.
+int main()
+{
+int a,b;
+printf("\n enter the number:");
+scanf("%d %d",&a,&b);
+if((a-b)%2==0)
+{
+printf("\nEVEN");
+}
+else
+{
+printf("\nODD");
+}
+return 0;
+}
